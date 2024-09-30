fa73795accbdfff539649a2825cfbe91c03f5ae5
kasparasdown <s235116@dtu.dk>
2024-09-30 18:18:09 +0200

    implemented simple_init

--- mm.c
+++ mm.c
@@ -20,11 +20,11 @@ typedef struct header {
 } BlockHeader;
 
 /* Macros to handle the free flag at bit 0 of the next pointer of header pointed at by p */
-#define GET_NEXT(p)    (void *) (p->next)    /* TODO: Mask out free flag */
-#define SET_NEXT(p,n)  p->next = (void *) n  /* TODO: Preserve free flag */
+#define GET_NEXT(p)    (void *) ( (uintptr_t)(p->next) & ~(uintptr_t)0x1 )   /* TODO: Mask out free flag */
+#define SET_NEXT(p,n)  p->next = (void *) ( ((uintptr_t)(n) & ~(uintptr_t)0x1) | ((uintptr_t)(p->next) & 0x1) )    /* TODO: Preserve free flag */
 #define GET_FREE(p)    (uint8_t) ( (uintptr_t) (p->next) & 0x1 )   /* OK -- do not change */
-#define SET_FREE(p,f)  /* TODO: Set free bit of p->next to f */
-#define SIZE(p)        (size_t) ( 0 ) /* TODO: Caluculate size of block from p and p->next */ 
+#define SET_FREE(p,f)  p->next = (void *) ( ((uintptr_t)(p->next) & ~(uintptr_t)0x1) | ( (f) ? 0x1 : 0x0 ) )  /* TODO: Set free bit of p->next to f */
+#define SIZE(p)        (size_t) ( (uintptr_t)GET_NEXT(p) - (uintptr_t)(p) - sizeof(BlockHeader) )/* TODO: Caluculate size of block from p and p->next */
 
 #define MIN_SIZE     (8)   // A block should have at least 8 bytes available for the user
 
@@ -38,8 +38,8 @@ static BlockHeader * current = NULL;
  *
  */
 void simple_init() {
-  uintptr_t aligned_memory_start = memory_start;  /* TODO: Alignment */
-  uintptr_t aligned_memory_end   = memory_end;    /* TODO: Alignment */
+  uintptr_t aligned_memory_start = (memory_start + 7) & ~(uintptr_t)0x7;  /* TODO: Alignment */
+  uintptr_t aligned_memory_end   = memory_end & ~(uintptr_t)0x7;    /* TODO: Alignment */
   BlockHeader * last;
 
   /* Already initalized ? */
@@ -47,6 +47,14 @@ void simple_init() {
     /* Check that we have room for at least one free block and an end header */
     if (aligned_memory_start + 2*sizeof(BlockHeader) + MIN_SIZE <= aligned_memory_end) {
       /* TODO: Place first and last blocks and set links and free flags properly */
+      first = (BlockHeader *)aligned_memory_start;
+      last = (BlockHeader *)(aligned_memory_end - sizeof(BlockHeader));
+
+      SET_NEXT(first, last);
+      SET_FREE(first, 1);
+
+      SET_NEXT(last, first);
+      SET_FREE(last, 0);
     }
     current = first;     
   } 
